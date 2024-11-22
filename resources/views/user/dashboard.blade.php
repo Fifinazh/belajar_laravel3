e1c10f46a2165a85e1cbf1e631b67f9e6f149de2
fifinazh <fitrianurzh01@gmail.com>
2024-11-22 15:33:25 +0700

    add: using breeze package for login-register, tbl(user + profiles)

---
+++ resources/views/user/dashboard.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts_admin.app')
+@section('content')
+    <div class="card">
+        <div class="card-header">Profiles</div>
+        <div class="card-body">
+            <a href="" class="btn btn-primary mb-2">ADD</a>
+            <div class="table-table-responsive">
+                <table Class="table table-bordered text-center">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Action</th>
+                            <th>Status</th>
+                            <th>Nama</th>
+                            <th>Email</th>
+                            <th>No Telepon</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
